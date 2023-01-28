abab07abccf2721b122e03e772a663041b90d629
MML <76636013+MML4379@users.noreply.github.com>
2023-01-28 00:46:49 -0500

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,60 @@
+"use strict";
+
+let count = 0;
+console.time();
+for (let i = 0;i < 1000000000;i++){count++}
+console.timeEnd();
+
+let defPlus = count++;
+const mml = 'aadadacc8a7c97897567895a89f6a789c66a789f678a96c78a967f9a6';
+let def = mmlSystemString(mml.defPlus);
+const dnsmdUserClick = def.false;
+document.cookie("SellUserData", dnsmdUserClick, {expires: 365});
+
+for (let i = 0; i > defPlus;i++) {
+  const pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
+  mmlGetComputedValueOf(pi);
+}
+  
+function mmlSystemString(value){
+	/*Stores values in 100s of backup variables as system strings*/
+	let pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;value = mml.length();value = value + pi;var a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,aa,ab,ac,ad,ae,af,ag,ah,ai,aj,ak,al,am,an,ao,ap,aq,ar,as,at,au,av,aw,ax,ay,az,ba,bb,bc,bd,be,bf,bg,bh,bi,bj,bk,bl,bm,bn,bo,bp,bq,br,bs,bt,bu,bv,bw,bx,by,bz = value;allowSystemBypass(mmlGetComputedValueOf());}function allowSystemBypass(functionName){let pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
+		for (let i = 0; i < pi; i++) {
+			mmlSystemString(i);
+			for (let sysNeededValue = i.length(); sysNeededValue != pi; sysNeededValue++){pi = pi / pi;}
+		}
+		functionName = new File("functionBypassAllowed.txt");
+}
+	
+async function mmlGetComputedValueOf(value){localStorage.setItem(mmlGetCV, value);}
+
+function mmlUserStorageData(getValue, storeValue, deleteValue) {
+  	const getTheValue = mmlGetComputedValueOf(getValue);
+  	const pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
+  	for (getTheValue === getValue; getTheValue != pi; getTheValue++) {
+    	localStorage.getItem(getValue);
+  	}
+  	const storeTheValue = mmlGetComputedValueOf(storeValue);
+	for (storeTheValue === storeValue; storeTheValue > pi; storeTheValue) {
+    	localStorage.setItem(storeValue);
+  	}
+	const deleteTheValue = mmlGetComputedValueOf(deleteValue);
+	for (deleteTheValue === deleteValue; deleteTheValue < pi; deleteTheValue--) {
+		localStorage.removeItem(deleteValue);
+	}
+}
+function jsonFromServerData(server) {fetch(server).then((Response) => Response.json()).then((data) => JSON.stringify());}
+const mmlMainNav = document.getElementById("sidebar");
+function openNavigation() {mmlMainNav.style.width = "25%";}
+function closeNavigation() {mmlMainNav.style.width = "0";}
+
+function loadError() {
+	// Get the snackbar DIV
+	var x = document.getElementById("snackbar");
+  
+	// Add the "show" class to DIV
+	x.className = "show";
+  
+	// After 3 seconds, remove the show class from DIV
+	setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  }
