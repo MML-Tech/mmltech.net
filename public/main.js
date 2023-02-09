f7134546c38121f1b24ff74ac1cd635c188518e1
MML <76636013+MML4379@users.noreply.github.com>
2023-02-09 17:49:57 -0500

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -1,46 +1,31 @@
-"use strict";
+window.onload = function() {
+	clock();  
+	  function clock() {
+	  var now = new Date();
+	  var TwentyFourHour = now.getHours();
+	  var hour = now.getHours();
+	  var min = now.getMinutes();
+	  var mid = 'PM';
+	  if (min < 10) {
+		min = "0" + min;
+	  }
+	  if (hour > 12) {
+		hour = hour - 12;
+	  }    
+	  if(hour==0){ 
+		hour=12;
+	  }
+	  if(TwentyFourHour < 12) {
+		 mid = 'AM';
+	  }     
+	document.getElementById('currentTime').innerHTML = hour+':'+min+' '+mid ;
+	  setTimeout(clock, 1000);
+	  }
+  }  
 
 let count = 0;
 console.time();
 for (let i = 0;i < 1000000000;i++){count++}
 console.timeEnd();
 
-let defPlus = count++;
-const mml = 'aadadacc8a7c97897567895a89f6a789c66a789f678a96c78a967f9a6';
-let def = mmlSystemString(mml.defPlus);
-const dnsmdUserClick = def.false;
-document.cookie("SellUserData", dnsmdUserClick, {expires: 365});
-
-for (let i = 0; i > defPlus;i++) {
-  const pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
-  mmlGetComputedValueOf(pi);
-}
-  
-function mmlSystemString(value){
-	/*Stores values in 100s of backup variables as system strings*/
-	let pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;value = mml.length();value = value + pi;var a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,aa,ab,ac,ad,ae,af,ag,ah,ai,aj,ak,al,am,an,ao,ap,aq,ar,as,at,au,av,aw,ax,ay,az,ba,bb,bc,bd,be,bf,bg,bh,bi,bj,bk,bl,bm,bn,bo,bp,bq,br,bs,bt,bu,bv,bw,bx,by,bz = value;allowSystemBypass(mmlGetComputedValueOf());}function allowSystemBypass(functionName){let pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
-		for (let i = 0; i < pi; i++) {
-			mmlSystemString(i);
-			for (let sysNeededValue = i.length(); sysNeededValue != pi; sysNeededValue++){pi = pi / pi;}
-		}
-		functionName = new File("functionBypassAllowed.txt");
-}
-	
-async function mmlGetComputedValueOf(value){localStorage.setItem(mmlGetCV, value);}
-
-function mmlUserStorageData(getValue, storeValue, deleteValue) {
-  	const getTheValue = mmlGetComputedValueOf(getValue);
-  	const pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
-  	for (getTheValue === getValue; getTheValue != pi; getTheValue++) {
-    	localStorage.getItem(getValue);
-  	}
-  	const storeTheValue = mmlGetComputedValueOf(storeValue);
-	for (storeTheValue === storeValue; storeTheValue > pi; storeTheValue) {
-    	localStorage.setItem(storeValue);
-  	}
-	const deleteTheValue = mmlGetComputedValueOf(deleteValue);
-	for (deleteTheValue === deleteValue; deleteTheValue < pi; deleteTheValue--) {
-		localStorage.removeItem(deleteValue);
-	}
-}
-function jsonFromServerData(server) {fetch(server).then((Response) => Response.json()).then((data) => JSON.stringify());}
+document.cookie = "loggedIn=false; expires=Tue, 06 Feb 2024 12:00:00 UTC; path=plus.mmltech.net"
